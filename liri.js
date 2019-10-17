d6eabd94bc87538ddbafebc5fdb3f2e371ae4801
jlramsey <jj@juniors-MBP.ndc.nasa.gov>
2019-10-17 17:19:45 -0400

    updates

--- liri.js
+++ liri.js
@@ -1,166 +1,152 @@
+require("dotenv").config();
 var keys = require("./keys.js");
-var Twitter = require("twitter");
+// Terminal Colors
+const chalk = require('chalk');
+// Calls to BandsInTown and OMDB
+var request = require('request');
+// Date Formatting
+var moment = require('moment');
+// Spotify
 var Spotify = require('node-spotify-api');
-var request = require("request");
-var fs = require("fs");
-var util = require("util");
+var spotify = new Spotify(keys.spotify);
 
+var fs = require('fs');
 
+let command = process.argv[2];
+let media_array = process.argv.slice(3);
+let media = media_array.join(" ");
 
-var command = process.argv[2];
+function doThings(command, media) {
+    switch (command) {
 
-
-
-// Twitter --------------------------------------------------------------------
-var twitter = new Twitter(keys.twitterKeys);
-
-// Get Tweet function
-function getTweets() {
-  twitter.get(
-    'statuses/user_timeline', { screen_name: "stellerzJay", count: 5 },
-    function(error, tweets, response) {
-      if (error) {
-      	console.log('Something broke...');
-        console.log(error);
-        return;
-      }
-
-      for (var i = 0; i < tweets.length; i++) {
-        console.log(`####################################################### `);
-
-        console.log("Posted at: " + tweets[i].created_at);
-        console.log("Tweet: " + tweets[i].text);
-        console.log("");
-      }
-
-    });
-};
-
-
-// Spotify -----------------------------------------------------------------------
-if (process.argv[4] === undefined || process.argv[4] === null) {
-    var song = process.argv[3];
-    // console.log(song);
-
-} else if (process.argv[4] !== undefined || process.argv[4] !== null) {
-    var song = process.argv[3] + ' ' + process.argv[4];
-    // console.log(song);
-    
-} else {
-    console.log('Please enter a valid song');
-    
+        case 'spotify-this-song':
+            spotifyThis(media); break;
+        case 'movie-this':
+            movieThis(media); break;
+        case 'concert-this':
+            concertThis(media); break;
+        case 'do-what-it-says':
+            doWhatItSays(); break;
+        default:
+            console.log("Invalid command. Please type any of the following commands:");
+            console.log(chalk.magenta("concert-this,"), chalk.cyan("spotify-this-song,"), chalk.magenta("movie-this,"), chalk.cyan("do-what-it-says"));
+    }
 }
 
-
-var spotify = new Spotify(keys.spotifyKeys);
-
-// Get Spotify songs function
-function getSpotifySong(song) {
-    if (song === undefined || song === null) {
-        song = 'The Sign';
-    } else {
-        
+function spotifyThis(media) {
+    // Default value
+    if (media == "") {
+        media = "All Star"
     }
 
+    // Search spotify API
     spotify
-    .search({ type: 'track', query: song, limit: 1 })
-    .then(function(response) {
-        
-        console.log(`#######################################################
-        Song: ${song} 
-        
-        Artist: ${response.tracks.items[0].album.artists[0].name}
-        
-        Album: ${response.tracks.items[0].album.name}
-        Preview URL: ${response.tracks.items[0].preview_url} `);
-        
+        .search({ type: 'track', query: media, limit: 1 })
+        .then(function (response) {
+            var song = response.tracks.items[0];
+            if (song != undefined) {
+                console.log();
+                console.log(chalk.green("***********Song Name**********"));
+                console.log(song.name);
 
+                console.log(chalk.green("******Artist or Artists:******"));
+                for (i = 0; i < song.artists.length; i++) {
+                    console.log(song.artists[i].name);
+                }
 
-    })
-    .catch(function(err) {
-      console.log(err);
-    });
-};
+                console.log(chalk.green("*********Preview Link*********"));
+                console.log(song.preview_url);
 
+                console.log(chalk.green("************Album*************"));
+                console.log(song.album.name);
+                console.log();
+            } else {
+                console.log("Can't find this song!")
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
 
-
-
-// OMDB ------------------------------------------------------------------------------
-
-// Get movies from OMDB API function 
-function getMovie(movie) {
-    if (movie === undefined || movie === null) {
-        movie = "Mr Nobody";
+function concertThis(media) {
+    // Default value
+    if (media == "") {
+        media = "Brockhampton"
     }
-    var requestUrl = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=" + keys.omdbapiKey.apiKey;
-    request.get({ url: requestUrl }, function (error, response, body) {
-
-        body = JSON.parse(body);
-        if (error || body.Response === "False" || body.Error === "Movie not found!") {
-            console.log("movie not found");
-            return;
-        }
-
-        console.log("Year of release: " + body.Year);
-        console.log("IMDB Rating: " + body.imdbRating);
-        for (var i = 0; i < body.Ratings.length; i++) {
-            if (body.Ratings[i].Source === "Rotten Tomatoes") {
-                console.log("Rotten Tomatoes Rating: " + body.Ratings[i].Value);
+    request("https://rest.bandsintown.com/artists/" + media + "/events?app_id=codingbootcamp", function (error, response, data) {
+        try {
+            var response = JSON.parse(data)
+            if (response.length != 0) {
+                console.log(chalk.green(`Upcoming concerts for ${media} include: `))
+                response.forEach(function (element) {
+                    console.log(chalk.cyan("Venue name: " + element.venue.name));
+                    if (element.venue.country == "United States") {
+                        console.log("City: " + element.venue.city + ", " + element.venue.region);
+                    } else {
+                        console.log("City: " + element.venue.city + ", " + element.venue.country);
+                    }
+                    console.log("Date: " + moment(element.datetime).format('MM/DD/YYYY'));
+                    console.log();
+                })
+            } else {
+                console.log(chalk.red("No concerts found."));
             }
         }
-        console.log("Country of production: " + body.Country);
-        console.log("Language of movie: " + body.Language);
-        console.log("Plot: " + body.Plot);
-        console.log("Actors: " + body.Actors);
+        catch (error) {
+            console.log(chalk.red("No concerts found."));
+        }
     });
-
 }
 
-
-// Get text from random.txt and pass it as a command to liri, function
-function doWhatItSays() {
-    fs.readFile('./random.txt', (err, data) => {
-        if (err) throw err;
-        console.log(data);
-        
-      });
-    
-
+function movieThis(media) {
+    // Default value
+    if (media == "") {
+        media = "Mr. Nobody"
     }
-    
-
-
-
-
-switch (command) {
-    case 'my-tweets':
-        util.log(`Getting tweets. . .`);  
-        getTweets();
-
-        break;
-
-    case 'spotify-this-song':
-        util.log('Getting song . . .')
-        getSpotifySong(song);
-
-        break;
-
-    case 'omdbGetMovie':
-        util.log('Getting movie . . .')
-        var movie = process.argv[3];
-        getMovie(movie);
-
-        break;
-
-    case 'do-what-it-says':
-        util.log('Getting text from random.txt . . .');
-        doWhatItSays(data);
-        
-        break;
-
-    default:
-        util.log('Please enter a valid command');
-        
-        break;
+    request("http://www.omdbapi.com/?apikey=trilogy&t=" + media, function (error, response, data) {
+        try {
+            var response = JSON.parse(data)
+            if (response.Title != undefined) {
+                console.log(chalk.green("*************Movie Name**************"));
+                console.log(response.Title);
+                console.log(chalk.green("*****************Year****************"));
+                console.log(response.Year);
+                console.log(chalk.green("***" + response.Ratings[0].Source + " Rating****"));
+                console.log(response.Ratings[0].Value);
+                // This is in case there's only ratings from one source
+                // at time of publication, ex: "Bad Times at the El Royale"
+                try {
+                    console.log(chalk.green("********" + response.Ratings[1].Source + " Rating*******"));
+                    console.log(response.Ratings[1].Value);
+                } catch { }
+                console.log(chalk.green("****************Country**************"));
+                console.log(response.Country);
+                console.log(chalk.green("***************Language**************"));
+                console.log(response.Language);
+                console.log(chalk.green("*****************Plot****************"));
+                console.log(response.Plot);
+                console.log(chalk.green("****************Actors***************"));
+                console.log(response.Actors);
+                console.log();
+            } else {
+                console.log(chalk.red("This movie not found."));
+            }
+        }
+        catch (error) {
+            console.log(chalk.red("This movie not found."));
+        }
+    });
 }
 
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function (err, response) {
+        if (err) {
+            console.log(err);
+        }
+        let params = (response.split(','));
+        doThings(params[0], params[1]);
+    })
+}
+
+doThings(command, media);
